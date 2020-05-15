867e7b739142c3fc1553f11a22ecf268d3556f89
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-05-15 17:46:31 -0600

    Create showformview function

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
-// Create variables targetting the relevant DOM elements here 👇
-
 var coverImage = document.querySelector("img.cover-image");
 var coverTitle = document.querySelector("h2.cover-title");
 var tagline = document.querySelector("h3.tagline");
 var randomCoverBtn = document.querySelector(".random-cover-button");
-
+var makeOwnCoverBtn = document.querySelector(".make-new-button");
+var formView = document.querySelector(".form-view");
+var homeView = document.querySelector(".home-view");
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
 
-// Add your event listeners here 👇
 
-randomCoverBtn.addEventListener('click', makeRandomCover);
-// window.addEventListener('onload', makeRandomCover);
-
-// Create your event handlers and other functions here 👇
+randomCoverBtn.addEventListener('click', newRandomCover);
+makeOwnCoverBtn.addEventListener('click', showFormView);
+// document.addEventListener('load', makeRandomCover);
 
 coverTitle.innerText = titles[getRandomIndex(titles)];
 coverImage.src = covers[getRandomIndex(covers)];
@@ -25,13 +23,12 @@ function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-function makeRandomCover() {
-  currentCover = new Cover(
-    covers[getRandomIndex(covers)],
-    titles[getRandomIndex(titles)],
-    descriptors[getRandomIndex(descriptors)],
-    descriptors[getRandomIndex(descriptors)]
-  );
+function newRandomCover() {
+  var cover = covers[getRandomIndex(covers)];
+  var title = titles[getRandomIndex(titles)];
+  var descriptor1 = descriptors[getRandomIndex(descriptors)];
+  var descriptor2 = descriptors[getRandomIndex(descriptors)];
+  currentCover = new Cover(cover, title, descriptor1, descriptor2);
   displayCover();
 }
 
@@ -39,4 +36,9 @@ function displayCover() {
   coverImage.src = currentCover.cover;
   coverTitle.innerText = currentCover.title;
   tagline.innerText = `A tale of ${currentCover.tagline1} & ${currentCover.tagline2}`;
+}
+
+function showFormView() {
+  formView.classList.remove("hidden");
+  homeView.classList.add("hidden");
 }
